0a7285f7b4cc74112d2d1d08271bc40bf1468895
Nashiria <35883865+Nashiria@users.noreply.github.com>
2022-03-26 07:41:48 +0300

    HW1

---
+++ HW1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using HW1CET322.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HW1CET322.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        public IActionResult Locations()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CET322
+## Projects:
+### [Homework 1](https://github.com/Nashiria/CET322/tree/main/HW1)
+#### Goal: 
+Improve the web page we created in the lecture in terms of visual and content, as well as fix the bugs explained in the lecture.
